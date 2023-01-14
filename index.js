687e6dfe2366aa1662a4740eaf85ff9239bf294e
Shiho(mia) <mia.can2021@gmail.com>
2023-01-14 00:37:41 -0800

    add mongoose set script to deploy

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ app.post('/:id', async(req, res) => {
 const PORT = process.env.PORT|| 8000
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DB)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`))
